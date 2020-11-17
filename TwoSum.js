46266780145dec1c8273803b973fac287ab35a9e
Harbans Grewal <harbans_grewal@hotmail.com>
2020-11-17 04:47:52 +0100

    Initial commit

--- TwoSum.js
+++ TwoSum.js
@@ -13,12 +13,13 @@
  Input: nums = [3,3], target = 6
  Output: [0,1]
  **/
-var twoSum = function(nums, target) {
-    for(var i=0;i<nums.length;i++){
-        for(var j = i+1;j<nums.length;j++){
-            if(nums[i]+nums[j] == target){
-                return [i,j]
-            }
+const twoSum = function (nums, target) {
+    let map = new Map;
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i]
         }
+        map.set(nums[i], i);
     }
 };
